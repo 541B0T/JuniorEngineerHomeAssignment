b1f5fea8eed316ee23f83a78f371ab14a66d4fdc
Tobias Traff <tobiastraff@live.se>
2022-09-07 15:27:34 +0200

    Name changes to maps and created Door Class.

--- src/main/java/com/example/demo/MontyHallApp.java
+++ src/main/java/com/example/demo/MontyHallApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MontyHallApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MontyHallApp.class, args);
 		System.out.println("test");
 	}
 
