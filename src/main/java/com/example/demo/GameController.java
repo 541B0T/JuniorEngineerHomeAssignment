446fc1a9a68d5134d7f4f3e140c2f87078cbbb2e
Tobias Traff <tobiastraff@live.se>
2022-09-11 21:06:40 +0200

    web game test created standard game that you can play

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GameController {
@@ -14,6 +16,27 @@ public class GameController {
     @GetMapping("/")
     public String start(Model model) {
         model.addAttribute("doors",gameShowHost.getAllDoors());
+        System.out.println("____WebGame____");
+        gameShowHost.CreateDoors();
+        gameShowHost.CalculateOds();
+        return "gameUI";
+    }
+    @GetMapping("/test")
+    public String test (){
+        return "test";
+    }
+    @PostMapping("/doorOne")
+    public String selectingDoorOne() {
+        gameShowHost.doors.get(0).setSelected(true);
+        gameShowHost.DoorListInfo();
+
+        return "gameUI";
+    }
+    @PostMapping("/doorTwo")
+    public String selectingDoorTwo() {
+        gameShowHost.doors.get(1).setSelected(true);
+        gameShowHost.DoorListInfo();
+
         return "gameUI";
     }
 }
