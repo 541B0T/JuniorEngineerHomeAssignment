c74677922a59107366771c6488411e30e9375d6b
Tobias Traff <tobiastraff@live.se>
2022-09-12 02:18:27 +0200

    web
    more work on the controller.

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -13,31 +13,35 @@ public class GameController {
 
     @Autowired
     GameShowHost gameShowHost;
-
     @GetMapping("/")
+    public String startGame(){
+        return "redirect:/game";
+    }
+
+    @GetMapping("/game")
     public String startGame(Model model) {
+        gameShowHost.doors.clear();
         model.addAttribute("doors",gameShowHost.getAllDoors());
         System.out.println("____WebGame____");
         gameShowHost.CreateDoors();
         gameShowHost.CalculateOds();
-        return "gameUI";
+
+        gameShowHost.doors.get(2).setOpen(true);
+        return "game";
     }
-    @GetMapping("/gameUI")
+    @GetMapping("/round")
     public String displayGameState(Model model){
         model.addAttribute("doors",gameShowHost.getAllDoors());
-        return "gameUI";
+        return "round";
     }
     @GetMapping("/{number}")
     public String selectingDoorTwo(@PathVariable int number) {
-
-        for (int i=0;i<3;i++){
-            if (i!=number-1){
-                gameShowHost.doors.get(i).setSelected(false);
-            }
-            else {
-                gameShowHost.doors.get(i).setSelected(true);
+        if (!gameShowHost.doors.get(number-1).isOpen()){
+            for (int i=0;i<3;i++){
+                gameShowHost.doors.get(i).setSelected(i == number - 1);
             }
         }
-        return "redirect:/gameUI";
+        gameShowHost.DoorListInfo();
+        return "redirect:/round";
     }
 }
